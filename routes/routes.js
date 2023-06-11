1ff7a9f1fb8efa1250cd587347859fbc30cf6e54
keshav sharma <ks98eng@gmail.com>
2023-06-11 22:21:21 +0530

    route added

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 const express  = require("express");
 const router = express.Router();
 
-router.get("/home",async (req,res,next) => {
+router.get("/",async (req,res,next) => {
     res.status(200).json({
         "message":"HELLO!!"
     })
